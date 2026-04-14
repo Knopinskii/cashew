4aab30eb8aad6c7a57959d18133ee76c135434d5
Vladimir <knopicanich@gmail.com>
2026-04-14 13:32:40 +0300

    added views and urls for finances and users

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from finance.models import IncomeCategory, ExpenseCategory, Income, Transaction
+from finance.serializers import IncomeCategorySerializer, ExpenseCategorySerializer, IncomeSerializer, TransactionSerializer
 
 # Create your views here.
+
+class BaseViewSet(viewsets.ModelViewSet):
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class IncomeCategoryViewSet(BaseViewSet):
+    queryset = IncomeCategory.objects.all()
+    serializer_class = IncomeCategorySerializer
+
+class ExpenseCategoryViewSet(BaseViewSet):
+    queryset = ExpenseCategory.objects.all()
+    serializer_class = ExpenseCategorySerializer
+
+class IncomeViewSet(BaseViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+class TransactionViewSet(BaseViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
